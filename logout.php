3db29e2f05576e6f48fa716bad6a905fd72999c6
Ryan Cather <ryan.cather@ed.act.edu.au>
2021-08-10 11:50:18 +1000

    Possibly fixed our logout and fixed the profilePic issue

--- logout.php
+++ logout.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
-unset($_SESSION["user_id"]);
-unset($_SESSION["name"]);
-unset($_SESSION["username"]);
-unset($_SESSION["level"]);
+//unset($_SESSION["user_id"]);
+//unset($_SESSION["name"]);
+//unset($_SESSION["username"]);
+//unset($_SESSION["level"]);
+session_unset();
 header("Location:index.php");
 ?>
